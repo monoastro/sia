c36b5cd0a5ad959999bf513d2c43e3f28a91b2db
Jen-ish <jenishpanta@gmail.com>
2024-07-17 20:18:09 +0545

    oopsie

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getUserInfo() {
+    const userInformation = localStorage.getItem("userInformation");
+    return userInformation ? JSON.parse(userInformation) : null;
+}
+
+// <button onClick={test} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors"> Test </button>
+
