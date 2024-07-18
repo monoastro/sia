5c4204ec4fa4375a59b7b669d1278123f9cfa920
Jen-ish <jenishpanta@gmail.com>
2024-07-18 21:05:19 +0545

    Is this enough Vercel?

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -19,32 +19,33 @@ export function NavigationItem({ id, imageUrl, name }: NavigationItemProps) {
   const params = useParams();
   const router = useRouter();
 
-  const onClick = () => {
+  const onClick = () => 
+  {
     router.push(`/application/${name}`);
   };
 
   return (
-    <ActionTooltip side="right" align="center" label={name}>
-      <button onClick={onClick} className="group relative flex items-center">
-        <div
-          className={cn(
-            "absolute left-0 bg-primary rounded-full transition-all w-[4px]",
-            params?.id !== id && "group-hover:h-[20px]",
-            params?.id === id ? "h-[36px]" : "h-[8px]"
-          )}
-        />
+	  <ActionTooltip side="right" align="center" label={name}>
+	  <button onClick={onClick} className="group relative flex items-center">
+	  <div
+	  className={cn(
+		  "absolute left-0 bg-primary rounded-full transition-all w-[4px]",
+		  params?.id !== id && "group-hover:h-[20px]",
+		  params?.id === id ? "h-[36px]" : "h-[8px]"
+	  )}
+	  />
 
-        <div
-          className={cn(
-            "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
-            params?.id === id &&
-              "bg-primary/10 text-primary rounded-[16px]"
-          )}
-        >
-          <Image fill src={imageUrl} alt="Tabs" />
-        </div>
-      </button>
-    </ActionTooltip>
+	  <div
+	  className={cn(
+		  "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
+		  params?.id === id &&
+			  "bg-primary/10 text-primary rounded-[16px]"
+	  )}
+	  >
+	  <Image fill src={imageUrl} alt="Tabs" />
+	  </div>
+	  </button>
+	  </ActionTooltip>
   );
 }
 
