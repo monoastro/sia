017d8b0ed7815825901f3ebe2f0aac636bea1a13
Jen-ish <jenishpanta@gmail.com>
2024-07-18 21:05:19 +0545

    Is this enough Vercel?

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,20 +1,19 @@
 //import  {currentProfile}  
-import { NavigationAction } from "@/components/navigation/navigation-action";
 import { Separator } from "@/components/ui/separator";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { NavigationItem } from "@/components/navigation/navigation-item";
-import { ModeToggle } from "@/components/mode-toggle";
+//import { ModeToggle } from "@/components/mode-toggle";
 import { tabs } from "@/lib/data";
+import React from "react";
 
-export const Sidebar = () => {
+export const Sidebar : React.FC = () => {
   return (
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full py-3" style={{ backgroundColor: 'rgba(22, 31, 115, 1)' }}>
         <NavigationItem
-          id={10}
+          id="10"
           name="dashboard"
           imageUrl="/shandi.svg"
         />
-      <NavigationAction/>
       <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
       <ScrollArea className="flex-1 w-full">
         {tabs.map((tab) => (
